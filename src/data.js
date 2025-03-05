91d6265fc85ac0abb7900ffe2cad86cf8a5eec4e
Ivan Korzhenetskyi <vanya200524@gmail.com>
2025-03-06 01:07:36 +0200

    GO

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const cardsData = [
+  {
+    id: 1,
+    title: "Марс",
+    description: "Червона планета, що має найбільший вулкан у Сонячній системі - Олімп Монс.",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Mars_Valles_Marineris_EDIT.jpg/274px-Mars_Valles_Marineris_EDIT.jpg"
+  },
+  {
+    id: 2,
+    title: "Сатурн",
+    description: "Відома своїми красивими кільцями, що складаються з крижаних уламків.",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Saturn_-_April_25_2016_%2837612580000%29.png/1200px-Saturn_-_April_25_2016_%2837612580000%29.png"
+  },
+  {
+    id: 3,
+    title: "Юпітер",
+    description: "Газовий гігант із великим червоним штормом, що триває століттями.",
+    image: "https://dovidka.biz.ua/wp-content/uploads/2015/10/yupiter-intersenie-fakti.jpg"
+  },
+  {
+    id: 4,
+    title: "Нептун",
+    description: "Найвіддаленіша планета Сонячної системи, відома своїми сильними вітрами.",
+    image: "https://cdn.drukarnia.com.ua/671817a35a00fc28df900176/images/articles/6726522c5a00fc28dfb1d356/HYv0438022.1668232216.jpeg"
+  },
+  {
+    id: 5,
+    title: "Венера",
+    description: "Найгарячіша планета з атмосферою, що складається переважно з вуглекислого газу.",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU3McSytZFcxAqzZHBxO7-cPJMqH2vfmx0nA&s"
+  }
+];
